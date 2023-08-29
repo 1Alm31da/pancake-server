1dbc05b24bbdf2809c9b6b18c26302394ac617b8
1Alm31da <igalmeida@deloitte.pt>
2023-08-29 19:49:52 +0100

    4th commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-      origin: "https://main--rad-biscuit-e4fd43.netlify.app",
+      origin: "*",
       methods: ["GET", "POST"],
     },
 });
